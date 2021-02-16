120a7d3fe72a8312e7ea246bfcad8b189772818f
Pamela Laitano <laitanop@yahoo.com>
2021-02-16 16:32:11 -0600

    header component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
-
+import Header from './components/header/header.component.jsx';
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route exact path="/shop" component={ShopPage} />
