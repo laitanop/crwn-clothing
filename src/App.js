a0fe5dafd6c8fef617f3a5bb71501d491eeef8bd
Pamela Laitano <laitanop@yahoo.com>
2021-05-31 13:19:24 -0600

    Action hidden

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            currentUser: null,
-        };
-    }
     unsubscribeFromAuth = null;
     componentDidMount() {
+        const { setCurrentUser } = this.props;
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
                 userRef.onSnapshot((snapShot) => {
-                    this.setState({
-                        currentUser: {
-                            id: snapShot.id,
-                            ...snapShot.data(),
-                        },
+                    setCurrentUser({
+                        id: snapShot.id,
+                        ...snapShot.data(),
                     });
                 });
             } else {
-                this.setState({ currentUser: userAuth });
+                setCurrentUser(userAuth);
             }
         });
     }
@@ -38,19 +33,30 @@ class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
-                <Header currentUser={this.state.currentUser} />
+                <Header />
                 <Switch>
                     <Route exact path="/" component={HomePage} />
-                    <Route exact path="/shop" component={ShopPage} />
+                    <Route path="/shop" component={ShopPage} />
                     <Route
                         exact
                         path="/signIn"
-                        component={SignInAndSignUpPage}
+                        render={() =>
+                            this.props.currentUser ? (
+                                <Redirect to="/" />
+                            ) : (
+                                <SignInAndSignUpPage />
+                            )
+                        }
                     />
                 </Switch>
             </BrowserRouter>
         );
     }
 }
-
-export default App;
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser,
+});
+const mapDispatchToProps = (dispatch) => ({
+    setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,4 @@
+import { CartActionType } from './cart.types';
+export const toggleCartHidden = () => ({
+    type: CartActionType.TOGGLE_CART_HIDDEN,
+});
